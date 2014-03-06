2e75cdf897d16f9760c6c24c98a4ffe29f66fbe5
skjelten@gmail.com <olesk75@users.noreply.github.com>
2014-03-06 00:34:41 +0000

    Arduino_Serial is now ready for first round.
    Needs TWI/I2C at 400khz to maintain 50fps raw transfer speed.

---  superled/Arduino_Serial/Arduino_Serial.ino
+++  superled/Arduino_Serial/Arduino_Serial.ino
@@ -1,127 +1,108 @@
 /*
-	This program has a simple mission: display anything sent to it over SPI on a 16x16 RGB led display
-	The Arduino is SPI slave
+	This program has a simple mission: display anything sent to it over I2C/TWI on a 16x16 RGB led display
+	This Arduino is I2C/TWI slave
+	
+	Some conventions in the code:
+	uint8_t is the preferred unsigned byte datatype.byte and unsigned char are avoided 
+	
+	To get maximum speed (which we need), we must tweak the Wire-library in file hardware/libraries/Wire/utility/twi.h. 
+		Near the top of the file you see :
+		Code:
+		#define TWI_FREQ 100000L
+		...CHANGE TO:...
+		#define TWI_FREQ 400000L
 
+		(restart Arduino UI for effect)
+		
+	Also, set TWBR = 2 (which should have same effect as above change)
+	
+	About 50 frames / second should be the result at 16MHz, more than the 30fps we need to be fluid
+	
 */
 #include <FastLED.h>		// The main library to help us control the LEDs - much faster than Adafruit's
-#include <SPI.h>			// To communicate with Arduino_Serial
-#include "pins_arduino.h"	// Mostly just for readability
+#include <Wire.h>			// To communicate with Arduino_WiFi
+
 #define NUM_LEDS 256		// Total number of LEDs
 #define COLOR_BYTES 3		// 3 bytes required per led for full color
-#define DATA_PIN 6			// Pin on the Arduino used to communicate with LED display
-#define ledPin 13			// On-board LED for Arduino UNO - used for debugging mostly - not required
+#define DATA_PIN 6			// Pin on the Arduino used to communicate with LED display (digital 6)
 
 #define BAUD_RATE 115200	// For logging
 
+#define TWI_slave  42
+
 CRGB leds[NUM_LEDS];		// FastLED class with .r .g and .b for each pixel - size: 3 bytes * NUM_LEDS
-char command;				// the command character we get from serial/WiFi		
+uint8_t command = ' ';		// the command character we get from serial/WiFi (' ' means unset)		
 
-/*
-char buf [100];				// SPI receive buffer
-volatile byte pos;			// position in buffer
-*/
-volatile boolean process_it;// flag to handle incoming data from SPI 
-
+uint8_t *ptr;				// Pointer used to traverse 2D array leds[][]
 
 void setup() {
-	// have to send on master in, *slave out*
-	pinMode(MISO, OUTPUT);
-
-	// turn on SPI in slave mode
-	SPCR |= _BV(SPE);
-
-	// get ready for an interrupt 
-	//pos = 0;   // buffer empty
-	process_it = false;
-
-	// now turn on interrupts
-	SPI.attachInterrupt();
+	Wire.begin(TWI_slave);									// Join I2C/TWI bus as slave
+	TWBR = 12;												// We need faster I2C/TWI...
+	Wire.onReceive(receiveEvent);							// register receive event handler - not used but must be present 
 	FastLED.addLeds<NEOPIXEL, DATA_PIN>(leds, NUM_LEDS);	// setting up pin 6 and the array 'leds' to work with FastLED library
-	boot_screen();					// Showing boot screen to test connection
+	boot_screen();											// showing boot screen to test connection
 	
-	Serial.begin(BAUD_RATE);		// Initializing serial communication
-	Serial.println(F("- SERIAL (SPI SLAVE) ARDUINO LIVE AGAIN!"));
-
+	Serial.begin(BAUD_RATE);								// initializing serial communication
+	Serial.println(F("- SERIAL (I2C/TWI SLAVE) ARDUINO LIVE AGAIN!"));
 	LEDS.setBrightness(128);	
 }
 
 void loop() { 
-	if (process_it)
-    	{
-    	Serial.print(F("I got: ")); Serial.println(command);
-    	/*
-		buf [pos] = 0;  
-		Serial.println (buf);
-		pos = 0;
-		process_it = false;*/
-		} 
-		/*
-	switch (command) {
 	
-		case 'G':	// 'G' means we are receiving a full screen buffer to display
-			{		  
-			Serial.write('A');	// GO code received - acknowledging
-			int read_bytes = Serial.readBytes( (char*)leds, NUM_LEDS * COLOR_BYTES);	// Full screen read
+	while(Wire.available()) {	// we got data waiting for us
+		command = Wire.read();
 		
-			if (read_bytes != NUM_LEDS * COLOR_BYTES) {	// We didn't get right number of bytes
-				Serial.write('E');						// Sending Error
-			} else {
-				Serial.write('R');						// Sending Received
-				FastLED.show();							// Showing new LED values
+		switch (command) {	
+			// 'G' is a special case, as in all other cases, the buffer is not really being used.
+			// But for G, we now will get one screen-full of data, 3 bytes per LED = 768 bytes
+			// However, to avoid RAM running out, we only do a quarter at a time
+			case 'G':	// 'G' means we are receiving a full screen buffer to display
+				{
+				//Serial.println("Getting full screen buffer");	// GO code received - acknowledging
+				uint8_t *ptr = (uint8_t *)&leds[0][0];			// pointer to CRGB array
+				for (int led_number = 0; led_number < NUM_LEDS * COLOR_BYTES; led_number++) {
+					while(!Wire.available()); 
+					*ptr = Wire.read();
+					ptr++;
+					}				
+				FastLED.show();					// Showing new LED values
+
+				break;
 				}
-			}	  
-			break;
-		case 'B':	// 'B' means we are receiving a new brightness value
-			{
-			Serial.write('A');	// Command code received - acknowledging
-			while (Serial.available() == 0) { }
-			int bright = Serial.read();
-			if (bright > 0 && bright < 256) {
-				Serial.write('A');						// Brightness value received OK
-				LEDS.setBrightness(bright);
-				FastLED.show();							// Showing new LED values
-				Serial.write('D');						// Confirming effect Done
-				
-			} 	else { Serial.write('E'); }				// Sending Error
-			break;
-			}
-		case 'T':	// 'T' means we are doing local tests of the pixels
-			{
-			Serial.write('A');	// Command code received - acknowledging
-			//test_pixelrun();
-			rainbow();
-			Serial.write('D');							// Confirming effect Done
-			break;
-			}
-		case 'Z':	// 'Z' means we are zeroing the LEDs (blanking the display)
-			{
-			Serial.write('A');	// Command code received - acknowledging
-			memset(leds, 0,  NUM_LEDS * sizeof(struct CRGB));
-			FastLED.show();
-			Serial.write('D');							// Confirming effect Done
-			break;
-			}
-		//default: // if nothing else matches, do this (for later)
-	}*/
+			case 'B':	// 'B' means we are receiving a new brightness value
+				{
+				Serial.println(F("Setting new brightness value"));	// Command code received - acknowledging
+				while(!Wire.available()); 
+				LEDS.setBrightness(Wire.read());
+				FastLED.show();					// Showing new LED values
+				Serial.println(F("Done"));							// Confirming effect Done
+				break;
+				}
+			case 'T':	// 'T' means we are doing local tests of the pixels
+				{
+				Serial.println(F("Hardware test requested from master"));	// Command code received - acknowledging
+				test_pixelrun();
+				//rainbow();
+				Serial.println(F("Hardware test complete"));							// Confirming effect Done
+				break;
+				}
+			case 'Z':	// 'Z' means we are zeroing the LEDs (blanking the display)
+				{
+				Serial.println(F("Zeroing display"));	// Command code received - acknowledging
+				memset(leds, 0,  NUM_LEDS * sizeof(struct CRGB));
+				FastLED.show();
+				Serial.println(F("Done"));							// Confirming effect Done
+				break;
+				}
+			//default: // if nothing else matches, do this (for later)
+		}
+	}
 }
 
-/*
-	SPI interrupt routine
-*/
-ISR (SPI_STC_vect)
-{
-command = SPDR;  // grab byte from SPI Data Register
- /* 
-	// add to buffer if room
-	if (pos < sizeof buf) {
-		buf [pos++] = c;
+// Empty request handler - as we take care of this manually and ignore the interrupt
+void receiveEvent(int howMany) {}
 
-		// example: newline means time to process buffer
-		if (pos > 75)	// NOTE: Not sure if we need this....
-		process_it = true;
-  
-	}  // end of room available*/
-}  // end of interrupt routine SPI_STC_vect
+
 /*
 Fades all the pixels to zero, step by step
 */
@@ -154,9 +135,9 @@ void test_pixelrun() {
 	Shows a rainbow pattern
 */
 void rainbow() {
-	byte red = 0;
-	byte green = 0;
-	byte blue = 0;
+	uint8_t red = 0;
+	uint8_t green = 0;
+	uint8_t blue = 0;
 
 	CRGB led_row[16];
 
@@ -183,7 +164,7 @@ void rainbow() {
 void boot_screen() {
 	int real_x;
 	int multiplier;
-	char stripes = sqrt(NUM_LEDS);		// Finding number of rows/columns - NOTE: *assuming* square display
+	uint8_t stripes = sqrt(NUM_LEDS);		// Finding number of rows/columns - NOTE: *assuming* square display
 	LEDS.setBrightness(255);		// Maximum brightness by default
 	
 	for (int x = 0; x < stripes + 5; x++) {
