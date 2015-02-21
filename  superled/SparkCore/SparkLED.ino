3bdeaeb699ac7aef545cacf900747456e0fc79d4
skjelten@gmail.com <olesk75@users.noreply.github.com>
2015-02-21 20:24:54 +0000

    First beta
    Works currently 99%
    
    Made significantly more robust to network interruptions. Will now reset and start listening for new connections after a one second timeout on the link.
    
    Only remaining issue is that it currently requires keepalives to keep link open, as it does not differentiate between a lost connection and a pause in output.

---  superled/SparkCore/SparkLED.ino
+++  superled/SparkCore/SparkLED.ino
@@ -32,6 +32,7 @@
 FASTLED_USING_NAMESPACE
 
 #include "application.h"
+#include <math.h>
 
 #define NUM_LEDS 256
 #define DATA_PIN 2
@@ -43,6 +44,10 @@ CRGB leds[NUM_LEDS];
 TCPServer server = TCPServer(2208);
 TCPClient client;
 char myIpAddress[24];
+bool connected;
+
+uint8_t readBuffer[2];
+int readStatus;
 
 void setup() 
 {
@@ -51,7 +56,6 @@ void setup()
     
     boot_screen(); //showing boot screen to show we're alive
     LEDS.setBrightness(255);
-    
     pinMode(int_led, OUTPUT);
     digitalWrite(int_led, LOW);
     
@@ -68,50 +72,90 @@ void loop()
 {
     Spark.process();
     
-    digitalWrite(int_led, LOW);     // Internal led OFF to verify visually
-    
     client = server.available();
     
     if (client.connected()) {
-        digitalWrite(int_led, HIGH);     // Internal led ON to verify visually
         
-        while(client.read() != 0x00 && client.read() != 'K') { 
+        connected = true;
+        
+        // TODO: Check if we can get out of sync here, reading the two bytes backwards
+        while ( readBuffer[0] != 0x00 ||    // where the first one is 0x00...
+                readBuffer[1] != 'K') {     // and the second one is b'K'
             Spark.process();
-            if (!client.connected()) client = server.available();   // and if we've lost connection, we try to reconnect
-        }
+            if (!connected) break;
+            readStatus = readByte(&readBuffer[0], 2);
+        }   
         
-        client.write('A'); // We ack connection and are ready to go
+        if (connected) client.write('A'); // We ack connection and are ready to go
         
-        while(client.connected()) {
-            while (client.read() != 0x00) {                             // Until we get the opening 0x00....
-                Spark.process();                                        // we check the cloud ...
-                if (!client.connected()) client = server.available();   // and if we've lost connection, we try to reconnect
-            }
-                
-            uint8_t code = client.read();
+        
+        while(connected) {
             
-            if (code == 'G') {      // getting screen update ("Go code" - \x00G)
-                client.write('A'); // We ack connection
-
+            while(true) {
+                readStatus = readByte(&readBuffer[0], 2);           // we keep reading byte pairs until...
                 
+                if ((readBuffer[0] == 0x00 && readStatus == 2) || !connected) break;
+                
+            }
+
+            if (!connected) break;      // if we've lost connection we break all the way back to root
+            
+            if (readBuffer[1] == 'G') {      // getting screen update ("Go code" - \x00G)
+                client.write('A'); // We ack connection
+                
+            
                 uint8_t *ptr = &leds[0][0];
                 
                 for (uint16_t pixel = 0; pixel < 768; pixel++) {
-                    while (client.available()<1) Spark.process(); 
-                    *ptr++ = client.read();
+                    do {
+                        readStatus = readByte((uint8_t *) ptr++, 1);
+                    } while (readStatus == 0 && connected);
+                    
+                    if(!connected) break;
                 }
                 
                 FastLED.show();
-                if (client.connected()) client.write('D'); // We tell the client we are Done
-                
-            } else if (code == 'T') {
-                // Hardware test pattern
+                client.write('D'); // We tell the client we are Done
+            } 
             
-            }   
+            if (readBuffer[1] == 'T') {   // Hardware test pattern
+                boot_screen();
+                if (connected) client.write('D'); // We tell the client we are Done
+            } 
+            
+            if (readBuffer[1] == 'B') {   // Set brightness (0-225)
+                uint8_t brightness;
+                
+                readByte(&brightness, 1);
+                LEDS.setBrightness(brightness);
+                if (connected) client.write('D'); // We tell the client we are Done
+            }
         }
     }
 }
 
+
+
+/* 
+    Reads a byte from the network client socket
+*/
+int readByte(uint8_t *destination, int bytesRead) {
+    long startTime = millis();
+    while(!client.available() && millis() - startTime < 1000) 
+        Spark.process();
+    
+    int statusCode = client.read((uint8_t *) destination, bytesRead);
+                
+    if (statusCode ==  -1) {        // client disconnected
+        no_connetion_warning();
+        client.flush();
+        client.stop();
+        connected = false;
+        Spark.process();
+        
+    }
+    return(statusCode);
+}
 
 /*
 	Boot screen - shows a sweeping line from left to right
@@ -142,4 +186,53 @@ void boot_screen() {
 		delay(10);
 	}
 	
+}
+
+/*
+    Gets complicated by the fact that we have a zig-zag pattern, so we need to flip every second line
+*/
+void no_connetion_warning() {
+    
+    uint16_t img_no_link[16] = {
+        0x1cF0, 0x1290, 0x1290, 0x1290, 0x12F0, 0x0000, 0x85C9, 0x852A, 0x852C, 0x852A, 0xF529, 0x0000, 0x0240, 0xFE7F, 0x0240, 0x0000
+        };
+        
+    for (uint8_t line = 0; line < 16; line++ ){
+        
+        if ( (line % 2) != 0) {    // every odd line...
+            img_no_link[line] = reverseBits(img_no_link[line]);
+        }
+        
+        for (uint8_t horis_led = 0; horis_led < 16; horis_led++) {
+            
+            if (img_no_link[line] & (uint16_t) pow(2, 15 - horis_led)) {
+                leds[(line * 16) + horis_led].red = 255;
+            } else {
+                leds[(line * 16) + horis_led].red = 0;
+            }  
+            
+            leds[(line * 16) + horis_led].green = 0;
+            leds[(line * 16) + horis_led].blue = 0;
+        }
+        
+    }
+        
+    
+    FastLED.show();
+}
+
+/* Function to reverse bits of num */
+uint16_t reverseBits(uint16_t num)
+{
+    uint8_t  NO_OF_BITS = sizeof(num) * 8;
+    uint16_t reverse_num = 0, temp;
+ 
+    for (uint8_t i = 0; i < NO_OF_BITS; i++)
+    {
+        temp = (num & (1 << i));
+        if(temp)
+            reverse_num |= (1 << ((NO_OF_BITS - 1) - i));
+    }
+  
+    return reverse_num;
 }
